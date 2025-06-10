ab7783f84364a70045eb421cbeb34d090ccbe0c3
Frost <2548345898@qq.com>
2025-06-10 17:34:18 +0800

    init

---
+++ vue-project/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button plain type="primary">朴素按钮</van-button>
+    <van-button plain type="success">朴素按钮</van-button>
+
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
